0b3333371b023c25bb26321434cc55e76dc7a1ef
jKathlein <jkathlein1@gmail.com>
2019-10-03 17:48:36 -0300

    criar projeto placar

---
+++ src/componentes/Placar.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './Placar.scss'
+
+const Placar = ({ time }) => {
+    return(
+        <div className="Placar">
+            <h1>Front</h1>
+            <h3></h3>
+        </div>
+    )
+}
+
+export default Placar;
---
+++ src/componentes/PlacarContainer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Placar from "./Placar";
+import './PlacarContainer';
+
+const PlacarContainer = () => {
+    return(
+        <div className="PlacarContainer">
+            <Placar />
+        </div>
+    )
+}
+
+export default PlacarContainer;
